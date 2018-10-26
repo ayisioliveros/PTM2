b7c6fda681072d866cd7af023db8a80259916d4b
maria <mholiveros@up.edu.ph>
2018-10-26 09:44:31 +0800

    Add new user controller and view Signed-off-by: maria <mholiveros@up.edu.ph>

--- TrackerModuleV1.0/Controllers/UsersController.cs
+++ TrackerModuleV1.0/Controllers/UsersController.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
 using System.Linq;
-using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using TrackerModuleV1._0.Data;
@@ -13,116 +10,73 @@ namespace TrackerModuleV1._0.Controllers
 {
     public class UsersController : Controller
     {
-        private PTMContex db = new PTMContex();
-
         // GET: Users
+        // GET: Account
         public ActionResult Index()
         {
-            return View(db.Users.ToList());
+            using (PTMContex db = new PTMContex())
+            {
+                return View(db.Users.ToList());
+            }
         }
 
-        // GET: Users/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            User user = db.Users.Find(id);
-            if (user == null)
-            {
-                return HttpNotFound();
-            }
-            return View(user);
-        }
-
-        // GET: Users/Create
-        public ActionResult Create()
+        public ActionResult Register()
         {
             return View();
         }
 
-        // POST: Users/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "UserId,FirstName,LastName,JobRole,ProjectNumber")] User user)
+        public ActionResult Register(User account)
         {
             if (ModelState.IsValid)
             {
-                db.Users.Add(user);
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                using (PTMContex db = new PTMContex())
+                {
+                    db.Users.Add(account);
+                    db.SaveChanges();
+                }
+                ModelState.Clear();
+                ViewBag.Message = account.FirstName + " " + account.LastName + " successfully registered.";
             }
-
-            return View(user);
+            return View();
         }
 
-        // GET: Users/Edit/5
-        public ActionResult Edit(int? id)
+        //Login
+        public ActionResult Login()
         {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            User user = db.Users.Find(id);
-            if (user == null)
-            {
-                return HttpNotFound();
-            }
-            return View(user);
+            return View();
         }
 
-        // POST: Users/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "UserId,FirstName,LastName,JobRole,ProjectNumber")] User user)
+        public ActionResult Login(User user)
         {
-            if (ModelState.IsValid)
+            using (PTMContex db = new PTMContex())
             {
-                db.Entry(user).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
+                var usr = db.Users.Where(u => u.FirstName == user.FirstName && u.LastName == user.LastName).FirstOrDefault();
+                if (usr != null)
+                {
+                    Session["UserId"] = usr.UserId.ToString();
+                    Session["FirstName"] = usr.FirstName.ToString();
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Username or Password is wrong.");
+                }
             }
-            return View(user);
+            return View();
         }
 
-        // GET: Users/Delete/5
-        public ActionResult Delete(int? id)
+        public ActionResult LoggedIn()
         {
-            if (id == null)
+            if (Session["UserId"] != null)
             {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                return View();
             }
-            User user = db.Users.Find(id);
-            if (user == null)
+            else
             {
-                return HttpNotFound();
+                return RedirectToAction("Login");
             }
-            return View(user);
-        }
-
-        // POST: Users/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            User user = db.Users.Find(id);
-            db.Users.Remove(user);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
         }
     }
 }
